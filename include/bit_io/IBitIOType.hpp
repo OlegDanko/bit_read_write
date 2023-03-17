85c4ebeabb3efcecab4f972fea3b24aeb1e390d3
Oleg Danko <olegdanko@outlook.com>
2023-03-17 02:06:07 +0200

    Added missing virtual destructors

--- include/bit_io/IBitIOType.hpp
+++ include/bit_io/IBitIOType.hpp
@@ -8,15 +8,18 @@ namespace bit_io {
 
 struct IWriteable {
     virtual void write(BitWriter&) const = 0;
+    virtual ~IWriteable() = default;
 };
 
 struct IReadable {
     virtual void read(BitReader&) = 0;
+    virtual ~IReadable() = default;
 };
 
 
 struct IBitIOType : IWriteable, IReadable {
     virtual size_t bit_size() const = 0;
+    virtual ~IBitIOType() = default;
 
 };
 
